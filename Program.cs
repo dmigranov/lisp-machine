d676e16ff4d782c4477ed488dad8c49e6004401b
Denis Migranov <d.migranov@g.nsu.ru>
2020-11-11 22:38:36 +0700

    wrote simple compiler call example but it doesn't work yet due to the usage of .net core

--- Program.cs
+++ Program.cs
@@ -6,23 +6,49 @@ class LispMachine
 { 
     static void Main(string[] args)
     {
-        bool isREPL = true; //TODO: parse args: REPL or build
+        bool isREPL = false; //TODO: parse args: REPL or build
 
         if (isREPL)
         {
             for(; ; )
             {
-
+                break;
             }
         }
         else
         {
             CSharpCodeProvider codeProvider = new CSharpCodeProvider();
-            ICodeCompiler icc = codeProvider.CreateCompiler();
             string fileName = "Out.exe"; //TODO: parse name from args
 
             System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = fileName;
+
+            string programText = @"
+                using System;
+
+                class HelloWorldClass
+                {
+                    static void Main(string[] args)
+                    {
+                        //TODO: parse и eval текст файла
+                        Console.WriteLine(""Test Test Test"");
+                    }
+                }";
+
+            CompilerResults results = codeProvider.CompileAssemblyFromSource(parameters, programText);
+
+            if (results.Errors.Count > 0)
+            {
+                Console.WriteLine("There were errors");
+                //https://docs.microsoft.com/en-us/troubleshoot/dotnet/csharp/compile-code-using-compiler
+            }
+
+            else
+                Console.WriteLine("Successfully compiled!");
         }
-        Console.WriteLine("Hello World!");
+
+        
     }
 }
