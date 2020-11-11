71bd0130bac5b3ebdac27a175bc861efab8dc2f1
Denis Migranov <d.migranov@g.nsu.ru>
2020-11-11 22:19:20 +0700

    добавлен скелет для Main: компиляция или REPL

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
 
 class LispMachine
 { 
     static void Main(string[] args)
     {
-        //todo: REPL
+        bool isREPL = true; //TODO: parse args: REPL or build
+
+        if (isREPL)
+        {
+            for(; ; )
+            {
+
+            }
+        }
+        else
+        {
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ICodeCompiler icc = codeProvider.CreateCompiler();
+            string fileName = "Out.exe"; //TODO: parse name from args
+
+            System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+        }
         Console.WriteLine("Hello World!");
     }
 }
