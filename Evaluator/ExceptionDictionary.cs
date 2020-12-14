2449640a58f517da29ed009fcb44ead557897044
Денис  Мигранов <d.migranov@g.nsu.ru>
2020-12-14 20:44:25 +0700

    скелет ExceptionDictionary

--- Evaluator/ExceptionDictionary.cs
+++ Evaluator/ExceptionDictionary.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-public class ExceptionDictionary 
+namespace LispMachine
 {
-    private Dictionary<Type, List<SExpr>> Dict = new Dictionary<Type, List<SExpr>>();
+    
+    public class ExceptionDictionary 
+    {
+        private Dictionary<Type, List<SExpr>> Dict = new Dictionary<Type, List<SExpr>>();
+    
+        private List<SExpr> Get(Type type)
+        {            
+            List<SExpr> ret;
+            //todo: проверка на наследование
+
+            return null;
+        }
+
+        private void Set(Type type, List<SExpr> value)
+        {
+            Dict[type] = value;
+        }
+    
+
+        public List<SExpr> this[Type index]
+        {
+            get => Get(index);
+            set => Set(index, value);
+        }
+    }
 
 
     
-
 }
