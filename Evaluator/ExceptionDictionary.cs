b47b4cdf32bbfde3a7f2b5a375cb55c248829e9f
Денис  Мигранов <d.migranov@g.nsu.ru>
2020-12-14 20:40:09 +0700

    создан класс ExceptionDictionary

---
+++ Evaluator/ExceptionDictionary.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ExceptionDictionary 
+{
+    private Dictionary<Type, List<SExpr>> Dict = new Dictionary<Type, List<SExpr>>();
+
+
+    
+
+}
