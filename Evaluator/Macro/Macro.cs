ac5e3d6aee87a5afe619faed14ffab24e5cb1085
Денис  Мигранов <d.migranov@g.nsu.ru>
2020-12-21 19:34:28 +0700

    pre branch

--- Evaluator/Macro/Macro.cs
+++ Evaluator/Macro/Macro.cs
@@ -7,9 +7,7 @@ namespace LispMachine
     public class Macro   //от кого он должен наслеоваться?
     {
         public List<SExprSymbol> MacroArguments { get; }
-        public SExpr Body { 
-            get;
-         } 
+        public SExpr Body { get; } 
         
         
         public Macro(List<SExprSymbol> args, SExpr body)
