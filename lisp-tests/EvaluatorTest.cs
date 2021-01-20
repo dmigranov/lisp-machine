61c5fda939c0908a436a6c24b852da52bea5c170
Денис  Мигранов <d.migranov@g.nsu.ru>
2021-01-20 14:47:35 +0700

    шаблон

---
+++ lisp-tests/EvaluatorTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LispMachine;
+using System.IO;
+
+namespace lisp_tests
+{
+
+    [TestClass]
+    public class EvaluatorTest
+    {
+        [TestMethod]
+        public void PrimitiveTest()
+        {
+
+
+        }
+
+
+    }
+
+}
