aa7d1354f3a52b471bcc798ba9d759e973bc5ed8
Денис  Мигранов <d.migranov@g.nsu.ru>
2021-01-20 14:44:12 +0700

    more

--- lisp-tests/ParserTest.cs
+++ lisp-tests/ParserTest.cs
@@ -1,12 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LispMachine;
+using System.IO;
 
 namespace lisp_tests
 {
     [TestClass]
     public class ParserTest
     {
+        [TestMethod]
+        public void PrimitiveTest()
+        {
+            var reader = new StringReader("5");
+            var parser = new SExprParser(reader);
 
+            var sexpr = parser.GetSExpression();
+            Assert.IsTrue(sexpr is SExprAbstractValueAtom);
+
+            sexpr = parser.GetSExpression();
+            Assert.IsNull(sexpr);
+
+            parser = new SExprParser(new StringReader(" 4 5 "));
+            sexpr = parser.GetSExpression();
+            Assert.IsTrue(sexpr is SExprAbstractValueAtom);
+
+            sexpr = parser.GetSExpression();
+            Assert.IsTrue(sexpr is SExprInt);
+            
+            if(sexpr is SExprInt atom)
+                Assert.AreEqual(5, atom.Value);
+        }
     }
 
 }
